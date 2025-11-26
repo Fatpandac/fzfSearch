a7cce8a2f00ebf852f8161ab3cfdae81e69f9a2b
fatpandac <i@fatpandac.com>
2025-11-26 16:40:59 +0800

    feat: support search file with regex and line number

--- src/utils/fzf.ts
+++ src/utils/fzf.ts
@@ -4,8 +4,47 @@ const CYCLE = "--cycle";
 
 export const SEARCH_FILE_CMD = (chooseFilePaths: string) => {
 	return `
-rg --files | fzf \
---preview "bat --color=always --plain {}" \
+fzf --phony --query "" \
+--delimiter ':' \
+--preview-window "+{2}-10" \
+--preview "
+  if [ -z "{2}" ]; then
+    bat --color=always --plain {1} 2>/dev/null || true
+  else
+    bat --color=always --plain --highlight-line {2} {1} 2>/dev/null || true
+  fi
+" \
+--bind 'change:reload:(
+  q={q};
+  if [ -z "$q" ]; then
+    rg --files
+  fi
+  name=$(printf "%s" $q: | cut -d: -f1);
+  line=$(printf "%s" $q: | cut -d: -f2);
+  res=$(rg --files | rg -i $name);
+
+  if [ -z "$res" ]; then
+	return
+  fi
+
+  if [ -z "$line" ]; then
+  	if [[ $q == *:* ]]; then
+    	echo $res | xargs -I{} echo {}:
+	else
+		echo $res
+	fi
+  else
+    echo "$res" | while read -r file; do
+      max_line=$(wc -l < $file | tr -d " ");
+      if [ "$line" -gt "$max_line" ]; then
+        echo "$file:$max_line"
+      else
+        echo "$file:$line"
+      fi
+    done
+  fi
+)' \
+--bind "start:reload:(rg --files)" \
 ${LAYOUT} \
 ${KEYMAPPING} \
 ${CYCLE} \
